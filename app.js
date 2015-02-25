f4a34616c91b51e4c0083b40a5fc09a31fa0c4c0
Mitch Denny <mitch@mitchdenny.com>
2015-02-25 17:21:31 +1100

    Got Node talking to the Ardunino via the johnny-five package.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+var five = require('johnny-five');
+var board = new five.Board();
+var led = null;
+
+board.on('ready', function() {
+  console.log('Board ready: Hello world!');
+});
